8c12f7428bfab00945720719808d57abc0f18b40
Adnan Krndzija <92021913+AdnanKrndzijaa@users.noreply.github.com>
2021-10-15 12:52:58 +0200

    Update Task8.java

--- week2/Task8.java
+++ week2/Task8.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 import java.util.Random;
 public class Task8 {
     public static void main(String [] args){
-                Scanner a=new Scanner(System.in);
+                Scanner reader=new Scanner(System.in);
                 Random random=new Random();
                 int x=random.nextInt(101);
                 System.out.println(x);
                 int i=1;
                 while(true){
                     System.out.println("Guess: ");
-                    int number=a.nextInt();
+                    int number=reader.nextInt();
                     if(number<x){
-                        System.out.println("Number is lesser, guess number "+i);
+                        System.out.println("Number is lesser, guess number " + i);
                     }
                     else if(number>x){
-                        System.out.println("Number is grater, guess number "+i);
+                        System.out.println("Number is grater, guess number " + i);
                     }
                     else{
                         System.out.println("Great. You are corrrect!");
